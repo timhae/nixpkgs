a252183754b90630e6877d9dd49bfa345612db89
Tim Haering <tim.haering@gmail.com>
2020-10-14 10:53:41 +0200

    final server implementation

--- pkgs/games/xmage/server.nix
+++ pkgs/games/xmage/server.nix
@@ -10,7 +10,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url    = "haering.dev/mage-server.zip";
-    sha256 = "14g4ll95hacv6s648r3av608bnj6r40fcqxz6z2kq38p08cd98zs";
+    sha256 = "1kaabkbb689xynijwp1hbv16r7vvnmc7s6yrgkibfnk836i1dwhk";
   };
 
   preferLocalBuild = true;
@@ -24,7 +24,8 @@ stdenv.mkDerivation rec {
     cp -rv ./* $out
 
     cat << EOF > $out/bin/xmage_server
-exec ${jdk8}/bin/java -Xms1g -Xmx4g -jar -Dfile.encoding=UTF-8 -Djava.security.policy=$out/config/security.policy -Dlog4j.configuration=file:$out/config/log4j.properties -Dconfig-path=$out/config/config.xml -Dplugin-path=$out/plugins/ -Dextension-path=$out/extension/ -jar $out/lib/mage-server-1.4.45.jar
+#!/usr/bin/env bash
+exec ${jdk8}/bin/java -Xms1g -Xmx4g -jar -Dfile.encoding=UTF-8 -Djava.security.policy=$out/config/security.policy -Dlog4j.configuration=file:$out/config/log4j.properties -Dconfig-path=$out/config/config.xml -Dplugin-path=$out/plugins/ -Dextension-path=$out/extension/ -Dmessage-path=$out/ -jar $out/lib/mage-server-1.4.45.jar
 EOF
 
     chmod +x $out/bin/xmage_server
