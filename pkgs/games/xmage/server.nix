23adaa897095f2042830552e391cac1c5f901c69
Tim Haering <tim.haering@gmail.com>
2021-06-29 09:46:10 +0200

    update xmage

--- pkgs/games/xmage/server.nix
+++ pkgs/games/xmage/server.nix
@@ -1,28 +1,35 @@
 { stdenv
-, fetchurl
-, jdk8
+, openjdk11
 , unzip
 }:
 
 stdenv.mkDerivation rec {
   name    = "xmage_server";
-  version = "1.4.45";
+  version = "1.4.48";
 
   src = /var/www/haering.dev/mage-server.zip;
 
   preferLocalBuild = true;
 
   unpackPhase = ''
-    ${unzip}/bin/unzip $src
+    ${unzip}/bin/unzip ${src}
   '';
 
   installPhase = ''
-    mkdir -p $out/bin
+    mkdir -p $out/bin $out/extension $out/plugins
     cp -rv ./* $out
 
     cat << EOF > $out/bin/xmage_server
 #!/usr/bin/env bash
-exec ${jdk8}/bin/java -Xms1g -Xmx4g -jar -Dfile.encoding=UTF-8 -Djava.security.policy=$out/config/security.policy -Dlog4j.configuration=file:$out/config/log4j.properties -Dconfig-path=$out/config/config.xml -Dplugin-path=$out/plugins/ -Dextension-path=$out/extension/ -Dmessage-path=$out/ -jar $out/lib/mage-server-1.4.45.jar
+    exec ${openjdk11}/bin/java -Xms1g -Xmx4g -jar \
+    -Dfile.encoding=UTF-8 \
+    -Djava.security.policy=$out/config/security.policy \
+    -Dlog4j.configuration=file:$out/config/log4j.properties \
+    -Dconfig-path=$out/config/config.xml \
+    -Dplugin-path=$out/plugins/ \
+    -Dextension-path=$out/extension/ \
+    -Dmessage-path=$out/ \
+    -jar $out/lib/mage-server-${version}.jar
 EOF
 
     chmod +x $out/bin/xmage_server
