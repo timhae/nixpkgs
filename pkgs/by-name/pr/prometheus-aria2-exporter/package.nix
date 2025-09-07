c809ae993888e22fc024b752288b2a3dfa5c15f3
Tim Häring <tim.haering@gmail.com>
2025-09-07 12:39:30 +0200

    prometheus-aria2-exporter: init at 0.1.2

---
+++ pkgs/by-name/pr/prometheus-aria2-exporter/package.nix
@@ -0,0 +1,27 @@
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+}:
+
+buildGoModule (finalAttrs: {
+  pname = "aria2-exporter";
+  version = "0.1.2";
+
+  src = fetchFromGitHub {
+    owner = "timhae";
+    repo = "aria2_exporter";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-tnUVKAZnoynCJNgs8n6EcQ5vMZyHjsgnHg/xpBTJ3Qc=";
+  };
+
+  vendorHash = "sha256-Ke4oqPqD9shMBmbz1MvOMpm24Y4b+uU3Avx108M9NeM=";
+
+  meta = {
+    description = "Prometheus exporter for Aria2";
+    homepage = "https://github.com/timhae/aria2_exporter";
+    license = lib.licenses.mit;
+    maintainers = [ lib.maintainers.timhae ];
+    mainProgram = "deluge-exporter";
+  };
+})
